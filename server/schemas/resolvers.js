5ce494c47b6ebecd405188de9a87691b5c4d1f23
TylerOliveira <55139667+TylerOliveira@users.noreply.github.com>
2022-03-09 19:49:19 -0800

    deploying

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,41 @@
+const { AuthenticationError } = require('apollo-server-express');
+const { User } = require('../models');
+const { signToken } = require('../utils/auth');
+const resolvers = {
+  Query: {
+    me: async (parent, args, context) => {
+        const userData = await User.findOne({ _id: context.user._id }).select('-__v -password');
+        return userData;
+    },
+  },
+  Mutation: {
+    addUser: async (parent, args) => {
+      const user = await User.create(args);
+      const token = signToken(user);
+      return { token, user };
+    },
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+      const correctPw = await user.isCorrectPassword(password);
+      const token = signToken(user);
+      return { token, user };
+    },
+    saveBook: async (parent, { newBook }, context) => {
+        const updatedUser = await User.findByIdAndUpdate(
+          { _id: context.user._id },
+          { $push: { savedBooks: newBook }},
+          { new: true }
+        );
+        return updatedUser;
+    },
+    removeBook: async (parent, { bookId }, context) => {
+        const updatedUser = await User.findByIdAndUpdate(
+          { _id: context.user._id },
+          { $pull: { savedBooks: { bookId }}},
+          { new: true }
+        );
+        return updatedUser;
+    },
+  },
+};
+module.exports = resolvers;

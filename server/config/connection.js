9e69ad9a4c3e3ba5bc3c55be2e02f10aba11a5c9
TylerOliveira <55139667+TylerOliveira@users.noreply.github.com>
2022-03-09 21:54:54 -0800

    updating

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost/googlebooks1',
+  process.env.MONGODB_URI || 'mongodb://localhost/book-search-engine',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
